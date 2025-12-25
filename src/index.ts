12cd52f73ecc3cf252a4e6d994535b8e8ad36dff
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2025-12-25 17:47:44 +0700

    deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
-import { PORT } from './utils/env';
+import { PORT, APP_URL } from './utils/env';
 
 app.listen(PORT, () => {
-  console.log(`Server MVC + Service E-Commerce jalan di http://localhost:${PORT}`);
+  console.log(`Server MVC + Service E-Commerce jalan di ${APP_URL}`);
   console.log(`Using DATABASE_URL: ${process.env.DATABASE_URL}`);
 });
